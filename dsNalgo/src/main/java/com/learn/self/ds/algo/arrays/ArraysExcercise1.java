881959b8bfcc814d262db078b4194d6d56dee5e6
Kishan Kumar <kishankumarc9@gmail.com>
2022-02-18 11:49:54 +0530

    first commit

---
+++ dsNalgo/src/main/java/com/learn/self/ds/algo/arrays/ArraysExcercise1.java
@@ -0,0 +1,47 @@
+package com.learn.self.ds.algo.arrays;
+
+
+/*
+
+    @author -- Kishan C
+
+    ##### Longest span with same sum in two binary array #####
+
+    arr1 = [0,1,1,0,1,0]
+    arr2 = [1,0,1,0,1,0]
+
+    Solution method 1 -- naive or simple solution
+
+     TC - O(n^2)
+     asc - O(1)
+    Solution method 2 -- Auxiliary Space solution
+
+*/
+
+public class ArraysExcercise1 {
+
+    static int []arr1 = new int[]{0,1,1,0,1,1,0};
+    static int []arr2 = new int[]{1,0,1,0,0,0,0};
+
+    static int longestCommonSum(int n){
+        int maxLen =0;
+        for(int i=0;i<n;i++){
+            int sum1=0; int sum2=0;
+            for(int j=i;j<n;j++){
+                sum1+=arr1[j];
+                sum2+=arr2[j];
+
+                if(sum1==sum2){
+                    int len =j-i+1;
+                    if(len>maxLen)
+                        maxLen = len;
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonSum(arr1.length));
+    }
+}
---
+++ dsNalgo/src/main/java/com/learn/self/ds/algo/arrays/ArraysExcercise2.java
@@ -0,0 +1,29 @@
+package com.learn.self.ds.algo.arrays;
+/*
+    @author -- Kishan C
+    #### Union and intersection of Two Sorted(in asc) arrays
+
+    Union Algorithm
+
+    arr1[] = {1.2.3.4.5.6}
+    arr2[] = {2,3,7,8}
+
+    1) step 1 take two pointers
+       i and j
+    2) itterate i and j
+        when i & j = 0
+        if arr1[i] < arr2[j]
+            add arr[i] to newly created array
+            increment i;
+        else arr1[i] > arr2[j]
+            add arr[j] to newly created array
+            increment j;
+        if arr[i] == arr[j]
+            add any and increment smaller array length
+*/
+
+public class ArraysExcercise2 {
+    public static void main(String[] args) {
+
+    }
+}
